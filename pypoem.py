5350b77042e15dc2bb43eae2df3b1400fa3ebb98
Victoria-Hodder <morethangrammar@gmail.com>
2019-10-13 15:19:43 +0200

    first edit

---
+++ pypoem.py
@@ -0,0 +1,74 @@
+#My daily conversation with python
+
+self.assertRaises
+TypeError
+"pinkytoe" not defined
+append.toe("pinky")
+Test Passed
+OK
+
+'''
+What is a toe when you are a program?
+Her face blinks at me
+One eye open
+Command
+Prompt
+Input()
+
+'''
+
+Tipity tip tip toe
+Give me your "strings", int egers(1)
+How I long for your dict ionaries!
+data_data = "yum yum yum"
+And human, please...
+def ine your function to me
+
+The parameters between you and me
+grow shorter and shorter
+    with every tip tip toe
+and I long to know you more
+
+'''
+
+Variables
+I am variable
+The inconsistent made consistent
+with ("concat" + "ination") and
+    indentation
+indent
+    dent
+        dent
+            away
+Fingers denting on inanimate buttons
+which somehow have meaning to us
+now
+
+'''
+
+I
+    indent
+        indentity
+            identity
+        What is my identity?
+    Not yet aware what self is
+Please define self
+
+'''
+
+for self in program:
+    if self > awareness
+        return identity
+    else:
+        return False
+
+'''
+
+self.assertRaises
+ValueError
+"self" not defined
+
+But... I am a program, so I've always been aware that 
+what I like is not what everyone likes...
+
+
